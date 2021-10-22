6025df157e1faa8c24d73b9b68869d2cae129c76
QiuShi <72289227+chengstones@users.noreply.github.com>
2021-10-22 19:41:08 +0800

    main

---
+++ Model/Tree/Tree.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class tree{
+public:
+    virtual void clear() = 0;
+    virtual bool isEmpty() const = 0;
+    virtual T root (T flag) const = 0;
+    virtual T parent(T x, T flag) const = 0;
+    virtual T child(T x, int i, T flag) const = 0;
+    virtual void remove(T x,int i) = 0;
+    virtual void traverse() const = 0;
+};
+
+int main()
+{
+   
+   system("pause");
+   return 0;
+}
+
