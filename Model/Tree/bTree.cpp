8c74ff7707af51aaea0c590ec1956c60b8db660f
QiuShi <72289227+chengstones@users.noreply.github.com>
2021-10-26 21:02:48 +0800

    main

--- Model/Tree/bTree.cpp
+++ Model/Tree/bTree.cpp
@@ -20,7 +20,42 @@ public:
 
 template<class T>
 class binaryTree:public bTree<T>{
+    friend void printTree(const binaryTree &t, T flag);
 
+private:
+    struct Node{
+        Node *left, *right;
+        T data;
+
+        Node():left(NULL),right(NULL){}
+        Node(T item, Node *L = NULL, Node *R = NULL):data(item),left(L),right(R){}
+        ~Node(){}
+    };
+    Node *root; // 二叉树的根节点 
+
+public:
+    binaryTree():root(NULL){}
+    binaryTree(T x){root = new Node(x);}
+    ~binaryTree();
+    void clear();
+    bool isEmpty() const;
+    T Root(T flag) const;
+    T lchild(T x, T flag) const;
+    T rchild(T x, T flag) const;
+    void delLeft(T x);
+    void delRight(T x);
+    void preOrder() const;
+    void midOrder() const;
+    void postOrder() const;
+    void levelOrder() const;
+    void createTree(T flag);
+    T parent(T x, T flag) const{return flag;}
+private:
+    Node* find(T x, Node *t) const;
+    void clear(Node* &t);
+    void preOrder(Node *t) const;
+    void midOrder(Node *t) const;
+    void postOrder(Node* t) const;
 };
 
 int main()
---
+++ Model/Tree/bTree_realize.cpp
@@ -0,0 +1,8 @@
+#ifndef BTREE_REALIZE_CPP
+#define BTREE_REALIZE_CPP
+
+#include "bTree.cpp"
+
+
+
+#endif //BTREE_REALIZE_CPP
